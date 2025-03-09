e0c193e1cb5b53546200d3bab7536e30d11e64a5
Danny <danny.f.gomez@gmail.com>
2025-03-08 22:49:46 -0500

    redirect to dash when logged in

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,7 +1,20 @@
 'use client';
+
+import React, { useContext, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Login from '../../../components/login/Login';
+import { AuthContext } from '../../../context/AuthContext';
 
 export default function LoginPage() {
+  const { currentUser } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (currentUser) {
+      router.push('/dashboard');
+    }
+  }, [currentUser, router]);
+
   return (
     <>
       <Login />
--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -1,7 +1,20 @@
 'use client';
+
+import React, { useContext, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Signup from '../../../components/signup/Signup';
+import { AuthContext } from '../../../context/AuthContext';
 
 export default function SignupPage() {
+  const { currentUser } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (currentUser) {
+      router.push('/dashboard');
+    }
+  }, [currentUser, router]);
+
   return (
     <div>
       <Signup />
