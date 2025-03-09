ab3252814471b1a5afa02d28d7c731bde033eb1e
Danny <danny.f.gomez@gmail.com>
2025-03-08 21:01:36 -0500

    add sign up text in login component

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -2,5 +2,10 @@
 import Login from '../../../components/login/Login';
 
 export default function LoginPage() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <div>Don&apos;t have an account? Sign Up</div>
+    </>
+  );
 }
