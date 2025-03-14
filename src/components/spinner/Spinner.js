fa13b641dab666979bef6b7d91a9f916cd73585f
Danny <danny.f.gomez@gmail.com>
2025-03-13 21:49:22 -0400

    create spinning loader

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={styles['dot-spinner']}>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+      <div className={styles['dot-spinner__dot']}></div>
+    </div>
+  );
+};
+
+export default Spinner;
