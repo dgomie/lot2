566f737665cc541b1aa25f4c56b403dbf729ce6a
Danny <danny.f.gomez@gmail.com>
2025-03-22 17:45:27 -0400

    create round page

---
+++ src/app/legions/[legionId]/[roundId]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RoundPage.module.css';
+
+const RoundPage = () => {
+  return (
+    <div>
+      <div>Round Page</div>
+    </div>
+  );
+};
+
+export default RoundPage;
