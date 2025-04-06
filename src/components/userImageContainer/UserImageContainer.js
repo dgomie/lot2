dffc67588fa9af72a9f3b18d1c61bd871bc4960f
Danny <danny.f.gomez@gmail.com>
2025-04-06 13:08:22 -0400

    create user image container component

---
+++ src/components/userImageContainer/UserImageContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './UserImageContainer.module.css';
+
+export const UserImageContainer = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div>User Images</div>
+    </div>
+  );
+};
