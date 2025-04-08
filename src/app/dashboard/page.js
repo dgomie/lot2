3ce955afba9c287489e53fb51a5dbdd903b1687a
Danny <danny.f.gomez@gmail.com>
2025-04-08 12:54:01 -0400

    remove title from dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,7 +4,6 @@ import React, { useContext } from 'react';
 import withAuth from '../../hoc/withAuth';
 import { AuthContext } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
-import Button from '@/components/button/Button';
 import styles from './dashboard.module.css';
 import DashboardFeed from '@/components/dashboardFeed/DashboardFeed';
 import BubbleMenu from '@/components/bubbleMenu/BubbleMenu';
@@ -20,24 +19,6 @@ const Dashboard = () => {
   return (
     <div className={styles.mainContainer}>
       {currentUser && <p>Welcome, {currentUser.username}!</p>}
-      {/* <div className={styles.buttonContainer}>
-        <Button
-          onClick={() => {
-            handleButtonClick('./create-legion');
-          }}
-        >
-          Create a Legion
-        </Button>
-        <Button
-          onClick={() => {
-            handleButtonClick('./legions');
-          }}
-        >
-          Join a Legion
-        </Button>
-      </div> */}
-      <div className={styles.title}>Your Active Legions</div>
-
       <DashboardFeed currentUserId={currentUser.uid} />
       <div className={styles.BubbleMenu}>
         <BubbleMenu />
