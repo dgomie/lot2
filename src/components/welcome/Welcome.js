8344fef5cbf57750d86705344e1f4870a222ef24
Danny <danny.f.gomez@gmail.com>
2025-04-13 15:37:29 -0400

    add descriptions to home page

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -39,25 +39,59 @@ const Welcome = () => {
 
   return (
     <div className={styles.mainContainer}>
-      <Image
-        src="/img/logo.svg"
-        alt="Legion of Tones Logo"
-        width={400}
-        height={400}
-      />
-      {isInstallable && (
-        <div className={styles.buttonContainer}>
-          <Button onClick={handleInstallClick} variant='black'>
-            <Image
-              src="/img/download.svg"
-              width={30}
-              height={30}
-              alt="Download App"
-            />
-            Download App
-          </Button>
+      <div className={styles.hero}>
+        <Image
+          src="/img/logo.svg"
+          alt="Legion of Tones Logo"
+          width={500}
+          height={500}
+        />
+        {isInstallable && (
+          <div className={styles.buttonContainer}>
+            <Button onClick={handleInstallClick} variant="black">
+              <Image
+                src="/img/download.svg"
+                width={30}
+                height={30}
+                alt="Download App"
+              />
+              Download App
+            </Button>
+          </div>
+        )}
+      </div>
+      <div className={styles.infoCard}>
+        <Image
+          src="/img/gnomes.png"
+          alt="Legion of Tones on Mobile"
+          width={400}
+          height={400}
+        />
+        <div className={styles.cardDescription}>
+          <h2>Share, Play, Discover</h2>
+          <p>
+            A social music game, where players form a legion and submit songs
+            that best match a musical prompt. Players receive a playlist of the
+            submissions and vote for the song that ranks supreme. Find new music
+            and share the songs you love with the Legion of Tones community!
+          </p>
         </div>
-      )}
+      </div>
+      <div className={styles.infoCard}>
+        <div className={styles.cardDescription}>
+          <h2>Bring the Legion with You</h2>
+          <p>
+            With iOS and Android optimization you can play on the go. Never miss
+            a deadline with notifications when songs and votes are due.
+          </p>
+        </div>
+        <Image
+          src="/img/mockup2.png"
+          alt="Legion of Tones on Mobile"
+          width={400}
+          height={400}
+        />
+      </div>
     </div>
   );
 };
