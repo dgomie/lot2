a66814504be5a4b33f1f60730f189ac438657492
Danny <danny.f.gomez@gmail.com>
2025-04-12 20:27:47 -0400

    add file structure for user settings modal

---
+++ src/components/userSettingsModal/UserSettingsModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './UserSettingsModal.module.css';
+
+export const UserSettingsModal = () => {
+  return (
+    <div>
+      <div>User Settings Modal</div>
+    </div>
+  );
+};
