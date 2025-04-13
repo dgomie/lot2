492da4956c2347b24e22178a70d98dff32f9b9f4
Danny <danny.f.gomez@gmail.com>
2025-04-12 20:21:21 -0400

    create file structure for admin settings modal

---
+++ src/components/adminSettingsModal/AdminSettingsModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './AdminSettingsModal.module.css';
+
+export const AdminSettingsModal = () => {
+  return (
+    <div>
+      <div>Admin Settings Modal</div>
+    </div>
+  );
+};
