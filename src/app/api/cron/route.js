fb2df8b69487ebf36f2156d49016b21e8ad04bbd
Danny <danny.f.gomez@gmail.com>
2025-04-15 20:09:34 -0400

    add cron auth for vercel

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -15,6 +15,14 @@ const normalizeDate = (date) => {
 };
 
 export async function GET(req, res) {
+
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
   try {
    const currentDate = normalizeDate(new Date());
 
