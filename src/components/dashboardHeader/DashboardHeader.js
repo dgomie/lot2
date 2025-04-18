aea146357968ffe75de7ccc57778082896b71d8a
Danny <danny.f.gomez@gmail.com>
2025-04-18 13:11:02 -0400

    add header for dashboard on mobile

--- src/components/dashboardHeader/DashboardHeader.js
+++ src/components/dashboardHeader/DashboardHeader.js
@@ -1,6 +1,44 @@
-import React from "react";
-import styles from './DashboardHeader.module.css'
+'use client'; // Ensure this is a client-side component
 
-export const DashboardHeader = () => {
-    return <div>DashboardHeader</div>
-}
+import React from 'react';
+import { useRouter } from 'next/navigation'; // Use next/navigation for client-side routing
+import styles from './DashboardHeader.module.css';
+import Image from 'next/image';
+
+export const DashboardHeader = ({ currentUserImage }) => {
+  const router = useRouter(); // Initialize the router
+
+  const handleLogoClick = () => {
+    if (router) {
+      router.push('/dashboard'); // Navigate to /dashboard
+    }
+  };
+
+  const handleIconClick = () => {
+    if (router) {
+      router.push('/profile');
+    }
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <Image
+        src={'/img/navlogo.svg'}
+        width={100}
+        height={100}
+        alt="Legion of Tones Logo"
+        onClick={handleLogoClick} // Add the click handler
+      />
+      <div className={styles.imageContainer}>
+        <Image
+          src={currentUserImage}
+          width={50}
+          height={50}
+          alt="user profile"
+          onClick={handleIconClick}
+          style={{borderRadius: '50%'}}
+        />
+      </div>
+    </div>
+  );
+};
