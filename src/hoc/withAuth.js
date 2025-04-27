fc9809c770bd1671c97302d39dbf1cecd3cca8e8
Danny <danny.f.gomez@gmail.com>
2025-04-27 19:31:15 -0400

    require email validation for withAuth

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,26 +1,62 @@
 'use client';
 
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { AuthContext } from '../context/AuthContext';
 import Loader from '@/components/loader/Loader';
+import { sendEmailVerification, getAuth } from 'firebase/auth';
 
 const withAuth = (WrappedComponent) => {
   const WithAuth = (props) => {
     const router = useRouter();
     const { currentUser, loading } = useContext(AuthContext);
+    const [verificationSent, setVerificationSent] = useState(false);
 
     useEffect(() => {
+      console.log('Auth state in withAuth:', { loading, currentUser }); // Debug log
+
       if (!loading && !currentUser) {
-        router.push('/auth/login');
+        console.log('Redirecting to login...');
+        router.push('/auth/login'); // Redirect to login if not authenticated
       }
     }, [loading, currentUser, router]);
 
-    if (loading || !currentUser) {
+    const handleResendVerification = async () => {
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      if (user) {
+        try {
+          await sendEmailVerification(user);
+          setVerificationSent(true);
+        } catch (error) {
+          console.error('Error resending verification email:', error);
+        }
+      } else {
+        console.error('No authenticated user found.');
+      }
+    };
+
+    if (loading) {
       return <Loader />;
     }
 
-    // Pass currentUser to the WrappedComponent
+    if (currentUser && !currentUser.emailVerified) {
+      return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <p>Your email is not verified. Please check your inbox.</p>
+          <button
+            onClick={handleResendVerification}
+            disabled={verificationSent}
+          >
+            {verificationSent
+              ? 'Verification Email Sent'
+              : 'Resend Verification Email'}
+          </button>
+        </div>
+      );
+    }
+
     return <WrappedComponent {...props} currentUser={currentUser} />;
   };
 
