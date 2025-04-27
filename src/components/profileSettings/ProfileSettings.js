cf606c4ed95361bc8685dcea88c897fbfb8b57cd
Danny <danny.f.gomez@gmail.com>
2025-04-27 17:45:59 -0400

    create profile settings component

---
+++ src/components/profileSettings/ProfileSettings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ProfileSettings.module.css';
+
+export const ProfileSettings = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div>Profile Settings</div>
+    </div>
+  );
+};
