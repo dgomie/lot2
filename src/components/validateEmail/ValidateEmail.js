908d527cc7213313d606501efbd3c8113bde23e9
Danny <danny.f.gomez@gmail.com>
2025-04-27 20:13:55 -0400

    create validateEmail component

---
+++ src/components/validateEmail/ValidateEmail.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import styles from './ValidateEmail.module.css';
+import Button from '../button/Button';
+import { sendEmailVerification, getAuth } from 'firebase/auth';
+
+export function ValidateEmail() {
+  const [verificationSent, setVerificationSent] = useState(false);
+
+  const handleResendVerification = async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      try {
+        await sendEmailVerification(user);
+        setVerificationSent(true);
+      } catch (error) {
+        console.error('Error resending verification email:', error);
+      }
+    } else {
+      console.error('No authenticated user found.');
+    }
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <p>Your email is not verified. Please check your inbox.</p>
+      <div className={styles.buttonContainer}>
+        <Button
+          variant="aquamarine"
+          onClick={handleResendVerification}
+          disabled={verificationSent}
+        >
+          {verificationSent
+            ? 'Verification Email Sent'
+            : 'Resend Verification Email'}
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,16 +1,15 @@
 'use client';
 
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { AuthContext } from '../context/AuthContext';
 import Loader from '@/components/loader/Loader';
-import { sendEmailVerification, getAuth } from 'firebase/auth';
+import { ValidateEmail } from '@/components/validateEmail/ValidateEmail';
 
 const withAuth = (WrappedComponent) => {
   const WithAuth = (props) => {
     const router = useRouter();
     const { currentUser, loading } = useContext(AuthContext);
-    const [verificationSent, setVerificationSent] = useState(false);
 
     useEffect(() => {
       console.log('Auth state in withAuth:', { loading, currentUser }); // Debug log
@@ -21,21 +20,6 @@ const withAuth = (WrappedComponent) => {
       }
     }, [loading, currentUser, router]);
 
-    const handleResendVerification = async () => {
-      const auth = getAuth();
-      const user = auth.currentUser;
-
-      if (user) {
-        try {
-          await sendEmailVerification(user);
-          setVerificationSent(true);
-        } catch (error) {
-          console.error('Error resending verification email:', error);
-        }
-      } else {
-        console.error('No authenticated user found.');
-      }
-    };
 
     if (loading) {
       return <Loader />;
@@ -43,17 +27,7 @@ const withAuth = (WrappedComponent) => {
 
     if (currentUser && !currentUser.emailVerified) {
       return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-          <p>Your email is not verified. Please check your inbox.</p>
-          <button
-            onClick={handleResendVerification}
-            disabled={verificationSent}
-          >
-            {verificationSent
-              ? 'Verification Email Sent'
-              : 'Resend Verification Email'}
-          </button>
-        </div>
+        <ValidateEmail />
       );
     }
 
