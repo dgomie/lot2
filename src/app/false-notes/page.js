d63d6eb0c7ff2fbc12165147d22c7f58a120330b
Danny <danny.f.gomez@gmail.com>
2025-06-05 23:56:27 -0400

    import dashboard card to falsenotes dashboard page

--- src/app/false-notes/page.js
+++ src/app/false-notes/page.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import styles from './false-notes.module.css';
 import withAuth from '@/hoc/withAuth';
+import DashboardCard from '@/components/dashboardCard/DashboardCard';
 
 const FalseNotesLeaguesDashboard = () => {
   return (
     <div className={styles.mainContainer}>
       <div>False Notes Leagues Dashboard</div>
+      <DashboardCard />
     </div>
   );
 };
